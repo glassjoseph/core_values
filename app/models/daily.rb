7b081186cae6ae6239a35f224ee6f8774c33d126
glassjoseph <glassjoseph@gmail.com>
2017-07-21 10:52:43 -0600

    Associate journals with dailies and users

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,7 +1,7 @@
 class Daily < ApplicationRecord
   belongs_to :user
   # belongs_to :supporter
-
+  has_many :journals
   has_many :taggings, :as => :taggable
   has_many :tags, through: :taggings
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,6 +1,8 @@
 class Journal < ApplicationRecord
-  belongs_to :user
-  belongs_to :daily, optional: true
+
   has_many :taggings, :as => :taggable
   has_many :tags, through: :taggings
+  belongs_to :user
+  belongs_to :daily, optional: true
+
 end
