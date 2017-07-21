f997d55ef58fc646edcc32d057ea69a5d0453b8b
glassjoseph <glassjoseph@gmail.com>
2017-07-20 18:02:29 -0600

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |n|
+
+  User.new(first_name: Faker::LordOfTheRings.character,
+           last_name: Faker::LordOfTheRings.character,
+           email: Faker::Internet.unique.email,
+           password: "password")
+  Sup.new(first_name: Faker::LordOfTheRings.character,
+           last_name: Faker::LordOfTheRings.character,
+           email: Faker::Internet.unique.email,
+           password: "password")
+
+
+end
