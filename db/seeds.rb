2e591b6a25b22d9d0e10c7c007e827452bdcdddf
glassjoseph <glassjoseph@gmail.com>
2017-07-22 21:59:36 -0600

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,14 +10,30 @@
 
 10.times do |n|
 
-  User.new(first_name: Faker::LordOfTheRings.character,
+  user = User.create!(first_name: Faker::LordOfTheRings.character,
            last_name: Faker::LordOfTheRings.character,
            email: Faker::Internet.unique.email,
            password: "password")
-  Sup.new(first_name: Faker::LordOfTheRings.character,
+  user.dailies.create!(goal: "Exercise?")
+  user.dailies.create!(goal: "Strengthen a relationship?")
+  user.dailies.create!(goal: "Study effectively?")
+  user.dailies.create!(goal: "Be patient?")
+
+  user.dailies.each do |daily|
+    daily.scores.create!(score: (rand(7) + 1 ), date: 1.day.ago.to_date)
+    daily.scores.create!(score: (rand(7) + 1 ), date: 2.days.ago.to_date)
+    daily.scores.create!(score: (rand(7) + 1 ), date: 3.days.ago.to_date)
+    daily.scores.create!(score: (rand(7) + 1 ), date: 4.days.ago.to_date)
+    daily.scores.create!(score: (rand(7) + 1 ), date: 5.days.ago.to_date)
+    daily.scores.create!(score: (rand(7) + 1 ), date: 6.days.ago.to_date)
+    daily.scores.create!(score: (rand(7) + 1 ), date: 7.days.ago.to_date)
+  end
+
+  Supporter.create(
+           first_name: Faker::LordOfTheRings.character,
            last_name: Faker::LordOfTheRings.character,
            email: Faker::Internet.unique.email,
-           password: "password")
+           )
 
 
 end
