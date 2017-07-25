63f8a03ff96dc91d7c0f351b48979dfd5e029976
glassjoseph <glassjoseph@gmail.com>
2017-07-25 14:49:50 -0600

    Enable daily deletion

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,9 +1,9 @@
 class Daily < ApplicationRecord
   belongs_to :user
-  # belongs_to :supporter
   has_many :taggings, :as => :taggable
   has_many :tags, through: :taggings
-  has_many :journals
-  has_many :scores
-  has_one :supporter
+  has_many :journals, dependent: :destroy
+  has_many :scores, dependent: :destroy
+  has_one :supporter, dependent: :destroy
+
 end
