f93aa1970ab3bf42e69199129f31811152e4de05
glassjoseph <glassjoseph@gmail.com>
2017-07-31 22:07:43 -0600

    Validate tagging by uniqueness of id and type

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,8 @@
 class Tagging < ApplicationRecord
   belongs_to :taggable, :polymorphic => true
   belongs_to :tag
+
+  validates_uniqueness_of :tag_id, :scope => [:taggable_id, :taggable_type]
+
+
 end
