aa5f55e7239420405e7348502e6522e33dc40b64
glassjoseph <glassjoseph@gmail.com>
2017-08-01 09:06:25 -0600

    Validate scores by score and date

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,7 @@
 class Score < ApplicationRecord
   belongs_to :daily
+  validates :date, :score, presence: true
+  validates_uniqueness_of :date, :scope => [:daily_id]
 
   scope :created_between, lambda {|start_date, end_date| where("date >= ? AND date <= ?", start_date, end_date )}
 end
