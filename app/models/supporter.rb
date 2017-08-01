a06171956b5f056387e3425b2b27e2465a5944b8
glassjoseph <glassjoseph@gmail.com>
2017-08-01 10:34:01 -0600

    Add SupporterMailer

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -1,7 +1,11 @@
 class Supporter < ApplicationRecord
-  belongs_to :user
-  belongs_to :daily
+  belongs_to :user, optional: true
+  belongs_to :daily, optional: true
 
   validates :first_name, :last_name, :email, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
 end
